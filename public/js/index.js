8508ce7ecf6f3133f890383174e1722a5de054c1
David Dias <daviddias.p@gmail.com>
2017-05-15 20:43:16 +0100

    feat: capture fetch requests

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then((registration) => {
+      console.log('registered')
+    })
+    .catch((err) => {
+      console.log('failed to register:', err)
+    })
+}
+
+document.querySelector('#show').addEventListener('click', () => {
+  const multihash = document.querySelector('#input').value
+  let imgElement = document.createElement('img')
+  imgElement.src = '/ipfs/' + multihash
+  // imgElement.src = 'https://ipfs.io/ipfs/' + multihash
+  document.querySelector('#display').appendChild(imgElement)
+})
---
+++ public/js/service-worker.js
@@ -0,0 +1,42 @@
+/* global self */
+'use strict'
+
+self.addEventListener('install', function (event) {
+  console.log('Installing')
+
+  // event.waitUntil(Promise.resolve().then(() => {
+  //  console.log('Install complete')
+  // }))
+
+  // Do I need to open caches?
+  event.waitUntil(self.skipWaiting())
+  /*
+    caches.open('my-cache-is-better-than-yours').then((cache) => {
+      return Promise.resolve().then(() => {
+        console.log('loaded caches')
+      })
+    }).then(() => {
+      console.log('Install complete')
+    })
+  )
+  */
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+  /*
+  event.waitUntil(Promise.resolve().then(() => {
+    console.log('Activate complete')
+  }))
+  */
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log('Handling fetch event for', event.request.url)
+
+  // skip cross-origin
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return
+  }
+  console.log('->')
+})
