489a8a3b840951d83004ec1d02dd60ca7095812f
Vasco Santos <vasco.santos@moxy.studio>
2018-05-03 17:18:21 +0100

    feat: Add toggle button for registering and unregistering the service worker

--- examples/use-from-another-page/public+sw/index.js
+++ examples/use-from-another-page/public+sw/index.js
@@ -3,22 +3,6 @@
 const { createProxyClient } = require('ipfs-postmsg-proxy')
 let node
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker-bundle.js')
-    .then((registration) => {
-      console.log('-> Registered the service worker successfuly')
-
-      node = createProxyClient({
-        addListener: navigator.serviceWorker.addEventListener.bind(navigator.serviceWorker),
-        removeListener: navigator.serviceWorker.removeEventListener.bind(navigator.serviceWorker),
-        postMessage: (data) => navigator.serviceWorker.controller.postMessage(data)
-      })
-    })
-    .catch((err) => {
-      console.log('-> Failed to register:', err)
-    })
-}
-
 document.querySelector('#id').addEventListener('click', async () => {
   if (!node) return alert('Service worker not registered')
   const { agentVersion, id } = await node.id()
@@ -27,10 +11,46 @@ document.querySelector('#id').addEventListener('click', async () => {
 
 document.querySelector('#show').addEventListener('click', () => {
   const multihash = document.querySelector('#input').value
-
-  if (multihash && multihash.length > 3) {
-    window.location.href = `/ipfs/${multihash}`
-  } else {
+  if (!node) {
+    alert('Service worker not registered')
+  } else if (!node || !multihash || multihash.length < 4) {
     alert(`invalid multihash received: ${multihash}`)
+  } else {
+    window.location.href = `/ipfs/${multihash}`
   }
 })
+
+document.querySelector('#serviceWorkerToggle').addEventListener('click', () => {
+  if (!node) {
+    register()
+  } else {
+    unregister()
+  }
+})
+
+const register = () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker-bundle.js')
+      .then((registration) => {
+        console.log('-> Registered the service worker successfuly')
+
+        node = createProxyClient({
+          addListener: navigator.serviceWorker.addEventListener.bind(navigator.serviceWorker),
+          removeListener: navigator.serviceWorker.removeEventListener.bind(navigator.serviceWorker),
+          postMessage: (data) => navigator.serviceWorker.controller.postMessage(data)
+        })
+      })
+      .catch((err) => {
+        console.log('-> Failed to register:', err)
+      })
+  }
+}
+
+const unregister = () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister()
+      node = null
+    })
+  }
+}
