cb81ad8d33bf86e4b676839394de0f6c14259376
Marcin Rataj <lidel@lidel.org>
2018-11-19 17:42:10 +0100

    fix: restore demo to functional state with js-ipfs 0.33.1

--- examples/use-from-another-page/README.md
+++ examples/use-from-another-page/README.md
@@ -1,3 +1,14 @@
 # Learn how to use the IPFS Service Worker from another page!
 
-> This is amazing!
+This directory contains two examples:
+
+- `public` - loads image from local HTTP gateway
+- `public+sw` - loads image from js-ipfs running in service worker
+
+How to play with it?
+
+1. `npm ci`
+2. `npm run build`
+3. `npm start`
+   - Open http://127.0.0.1:9042/public/
+   - Open http://127.0.0.1:9042/public%2Bsw/
--- examples/use-from-another-page/webpack.config.js
+++ examples/use-from-another-page/webpack.config.js
@@ -1,9 +1,27 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
+  mode: 'development', // use only during development
+  devtool: 'inline-source-map', // use only during development
   entry: {
-    'service-worker-bundle': './src/index.js',
+    'service-worker-bundle': './sw/index.js',
     bundle: './public+sw/index.js'
   },
+  optimization: {
+    minimizer: [
+      // Default flags break js-ipfs: https://github.com/ipfs-shipyard/ipfs-companion/issues/521
+      new UglifyJsPlugin({
+        parallel: true,
+        extractComments: true,
+        uglifyOptions: {
+          compress: { unused: false },
+          mangle: true
+        }
+      })
+    ]
+  },
   output: {
+    path: path.resolve(__dirname),
     filename: 'public+sw/[name].js'
   },
   node: {
